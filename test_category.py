b29b40373ba7bcdc948cf1f4db9adbafc779ff27
mades eladrian <mades.eladrian@gmail.com>
2024-06-09 17:18:24 -0300

    refact using faker library

--- test_category.py
+++ test_category.py
@@ -1,43 +1,54 @@
+from faker import Faker
 import pytest
 from uuid import UUID, uuid4
 
 from category import Category
 
+
 class TestCategory:
+    faker = Faker()
+
+    category_id: UUID = uuid4()
+    name: str = faker.word()
+    description: str = faker.sentence(nb_words=10)
+
     def test_name_is_required(self):
         with pytest.raises(TypeError, match="missing 1 required positional argument: 'name'"):
             Category()
 
     def test_name_must_have_less_than_255_characters(self):
         with pytest.raises(ValueError, match="name can not be longer than 255 characters"):
-            Category(name="a" * 256)
+            Category(name=self.faker.sentence(100))
 
     def test_category_must_be_created_with_id_as_uuid_by_default(self):
-        category = Category(name="Category")
+        category = Category(name=self.name)
+
         assert category.id is not None
         assert isinstance(category.id, UUID)
 
     def test_created_category_with_default_values(self):
-        category = Category(name="Category")
-        assert category.name == "Category"
+        category = Category(name=self.name)
+
+        assert category.name == self.name
         assert category.description == ""
         assert category.is_active == True
 
     def test_category_is_created_as_active_by_default(self):
-        category = Category(name="Category")
+        category = Category(name=self.name)
+
         assert category.is_active is True
 
     def test_category_is_created_with_provided_values(self):
-        category_id = uuid4()
         category = Category(
-            id=category_id,
-            name="Category",
-            description="Category Description",
-            is_active=False,
+            id=self.category_id,
+            name=self.name,
+            description=self.description,
+            is_active=False
         )
-        assert category.id == category_id
-        assert category.name == "Category"
-        assert category.description == "Category Description"
+
+        assert category.id == self.category_id
+        assert category.name == self.name
+        assert category.description == self.description
         assert category.is_active is False
 
     def test_can_not_create_category_with_empty_name(self):
@@ -49,18 +60,35 @@ class TestCategory:
             Category(name=None)
 
 class TestUpdateCategory:
-    def test_update_category_with_name_and_description(self):
-        category = Category(name="Filme", description="Filme Description")
-        category.update_name_and_description(name="Séries", description="Séries Description")
+    faker = Faker()
 
-        assert category.name == "Séries"
-        assert category.description == "Séries Description"
+    name: str = faker.word()
+    description: str = faker.sentence(nb_words=10)
+
+    def test_update_category_with_name_and_description(self):
+        updated_name = self.faker.word()
+        updated_description = self.faker.sentence(nb_words=7)
+
+        category = Category(name=self.name, description=self.description)
+        category.update_name_and_description(
+            name=updated_name,
+            description=updated_description
+        )
+
+        assert category.name == updated_name
+        assert category.description == updated_description
 
     def test_update_category_with_invalid_name_raises_exception(self):
-        category = Category(name="Filme", description="Filme Description")
+        updated_name = self.faker.sentence(100)
+        updated_description = self.faker.sentence(nb_words=7)
+
+        category = Category(name=self.name, description=self.description)
 
         with pytest.raises(ValueError, match="name can not be longer than 255 characters"):
-            category.update_name_and_description(name="a" * 256, description="Séries Description")
+            category.update_name_and_description(
+                name=updated_name,
+                description=updated_description
+            )
 
     def test_can_not_update_category_with_empty_name(self):
         with pytest.raises(ValueError, match="name can not be empty or null"):
@@ -71,26 +99,39 @@ class TestUpdateCategory:
             Category(name=None)
 
 class TestActivateCategory:
+    faker = Faker()
+
+    name: str = faker.word()
+    description: str = faker.sentence(nb_words=10)
+    
     def test_activate_inactive_category(self):
-        category = Category(name="Filme", description="Filme Description", is_active=False)
+        category = Category(
+            name=self.name,
+            description=self.description,
+            is_active=False
+        )
         category.activate()
 
         assert category.is_active is True
 
     def test_activate_activate_category(self):
-        category = Category(name="Filme", description="Filme Description")
+        category = Category(name=self.name, description=self.description)
         category.activate()
 
         assert category.is_active is True
 
     def test_deactivate_inactive_category(self):
-        category = Category(name="Filme", description="Filme Description", is_active=False)
+        category = Category(
+            name=self.name,
+            description=self.description,
+            is_active=False
+        )
         category.deactivate()
 
         assert category.is_active is False
 
     def test_deactivate_activate_category(self):
-        category = Category(name="Filme", description="Filme Description")
+        category = Category(name=self.name, description=self.description)
         category.deactivate()
 
         assert category.is_active is False
